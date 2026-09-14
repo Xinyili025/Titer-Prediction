52ce7b72d9eb33b02a07c4472621f89ea9ef51c9
Xinyili025 <lixinyi025@gmail.com>
2026-09-14 10:57:27 +0800

    Update Run_all.R

--- R/Run_all.R
+++ R/Run_all.R
@@ -3,19 +3,34 @@
 setwd("/Users/lixinyi/Desktop/Titer-Prediction-main")
 
 # R scripts are sourced in order
+# Setup
 source("R/00_Setup.R")
-source("R/01_Cross_validation.R")
-source("R/02_Gold_standard_models.R")
-source("R/03_Shared_parameter_models.R")
-source("R/04_Od_curves.R")
-source("R/05_Fig01_od_curves.R")
-source("R/06_Apptable_1.R")
+
+# Gold standard & shared models
+source("R/01_Gold_standard_models.R")
+source("R/02_Shared_parameter_models.R")
+
+# OD curves, figure 1, appendix figure 1, appendix figure 2, and appendix table 1
+source("R/03_Od_curves.R")
+source("R/04_Fig01_od_curves.R")
+source("R/05_Apptable01.R")
+
+# Cross-validation for optimal models & appendix table 2
+source("R/06_Cross_validation.R")
+
+# Optimal combinations & appendix figure 3
 source("R/07_Optimal_combinations.R")
-source("R/08_Optimal_models.R")
-source("R/09_Appfig03_optimal_models.R")
+source("R/08_Optimal_combinations_curves.R")
+source("R/09_Appfig03_optimal_combinations.R")
+
+# Ratio & Figure 2
 source("R/10_Ratio.R")
 source("R/11_Fig02_ratio.R")
+
+# Assay-capped scenario & Figure 3
 source("R/12_Assay_capped_scenario.R")
-source("R/12_Fig03_assay_capped_scenario.R")
+source("R/13_Fig03_assay_capped_scenario.R")
+
+# Real-world scenario & Appendix Figure 4
 source("R/14_Real_world_scenario.R")
-source("R/15_Appfig4_real_world_scenario.R")
+source("R/15_Appfig04_real_world_scenario.R")
