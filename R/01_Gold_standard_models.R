bed1d998d23360936df14c36a60aef610c622670
Xinyili025 <lixinyi025@gmail.com>
2026-09-16 09:17:42 +0800

    Update 01_Gold_standard_models.R

--- R/01_Gold_standard_models.R
+++ R/01_Gold_standard_models.R
@@ -1,3 +1,5 @@
+set.seed(123)
+
 # Extract datasets with full dilution series from the sets_od list
 full_sets <- list(cobovax_ancestral=sets_od$cobovax_ancestral$n12[[1]],
                   cobovax_omicron=sets_od$cobovax_omicron$n12[[1]],
